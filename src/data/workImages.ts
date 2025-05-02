091f2ef22cc0d26461c378da97ae541467929810
Sneha <speakingofsneha@gmail.com>
2025-05-02 20:28:51 +1000

    fix paths

--- src/data/workImages.ts
+++ src/data/workImages.ts
@@ -6,52 +6,52 @@ interface WorkImageData {
 
 const workImages: WorkImageData[] = [
   {
-    url: " images/work/datavis.png",
+    url: "/images/work/datavis.png",
     id: "data vis",
     category: "code design"
   },
   {
-    url: " images/work/olympal.png",
+    url: "/images/work/olympal.png",
     id: "olympal",
     category: "kdesign",
   },
   {
-    url: " images/work/anu.png",
+    url: "/images/work/anu.png",
     id: "anu",
     category: "code design",
   },
   {
-    url: " images/work/litbox.png",
+    url: "/images/work/litbox.png",
     id: "lit-box",
     category: "research design",
   },
   {
-    url: " images/work/sapasa.png",
+    url: "/images/work/sapasa.png",
     id: "sapasa",
     category: "design",
   },
   {
-    url: " images/work/qualres.png",
+    url: "/images/work/qualres.png",
     id: "qual res",
     category: "research",
   },
   {
-    url: " images/work/kolam.png",
+    url: "/images/work/kolam.png",
     id: "kolam",
     category: "research",
   },
   {
-    url: " images/work/zine.png",
+    url: "/images/work/zine.png",
     id: "zine",
     category: "research design",
   },
   {
-    url: " images/work/mahabs.png",
+    url: "/images/work/mahabs.png",
     id: "mahabs",
     category: "code design",
   },
   {
-    url: " images/work/misc.png",
+    url: "/images/work/misc.png",
     id: "misc.",
     category: "design research",
   },
