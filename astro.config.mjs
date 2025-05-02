1441983b4793cf96f39761c2cac3e036f600d154
Sneha <speakingofsneha@gmail.com>
2025-05-02 16:27:33 +1000

    ho

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,9 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+// https://chatgpt.com/share/68145399-11c8-800f-be1d-6039fd868147
 export default defineConfig({
   integrations: [react()],
-  site: 'https://speakingofsneha.github.io/sneha.work.github.io/', // your deployed URL
+  site: 'https://speakingofsneha.github.io/sneha.work.github.io/', // your deployed URL 
   base: '/sneha.work.github.io/', // your GitHub repo name with slashes
 });
