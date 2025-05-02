170acef464b007ce82da5ae94d41872c0050c72c
Sneha <speakingofsneha@gmail.com>
2025-05-02 16:18:36 +1000

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://sneha.work.github.io/my-project/', 
+  site: 'https://speakingofsneha.github.io/sneha.work.github.io/', // your deployed URL
+  base: '/sneha.work.github.io/', // your GitHub repo name with slashes
 });
