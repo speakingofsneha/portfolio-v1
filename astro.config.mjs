eff1ab1fb4478b40d0ea24593c0452f1d0629b08
Sneha <speakingofsneha@gmail.com>
2025-05-02 12:23:24 +1000

    Pls god

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://sneha.work.github.io',
+  site: 'https://sneha.work.github.io/my-project/', 
+  base: '/my-project/', 
 });
