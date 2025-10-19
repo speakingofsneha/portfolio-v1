4b8021322e6d8b1eca0ea72ca38e033e35c54d0f
Sneha <speakingofsneha@gmail.com>
2025-10-19 18:27:56 +1100

    remove cname

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import react from "@astrojs/react";
 // https://chatgpt.com/share/68145399-11c8-800f-be1d-6039fd868147
 export default defineConfig({
   integrations: [react()],
-  site: 'https://sneha.design', 
+  site: 'https://speakingofsneha.github.io/portfolio-v1', 
 });
